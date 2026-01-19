e20d7f39aaccec8dfc922291c517326aa65ba2ed
Daniel Torres <daniel.torres@joinplank.com>
2026-01-19 16:31:26 -0300

    fix: fix import

--- src/components/bucket.ts
+++ src/components/bucket.ts
@@ -1,6 +1,6 @@
 import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
-import { getDefaultTags } from "../utils";
+import { getDefaultTags } from "../utils/tags";
 
 export interface BucketComponentConfig {
   readonly name: string;
